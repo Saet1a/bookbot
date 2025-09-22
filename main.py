ed2a73326fe4e4466b9550b64c101c3ec1186870
Saet1a <joaorogeroliveira@gmail.com>
2025-09-22 13:19:16 -0300

    Refactor: move statistical function to stats.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-
+from stats import count_words
 def main():
     path = 'books/frankenstein.txt'
     text = get_book_text(path)
     number_of_words = count_words(text)
-    print(f'{number_of_words} words found in the document')
+    print(f'Found {number_of_words} total words')
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
-def count_words(text_to_count):
-    words_number = text_to_count.split()
-    return len(words_number)
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(text_to_count):
+    words_number = text_to_count.split()
+    return len(words_number)
