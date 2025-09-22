d1ba9386098a545478d9afbd282ea314454ec949
Saet1a <joaorogeroliveira@gmail.com>
2025-09-22 15:51:55 -0300

    Add function to print the report

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-from stats import count_words,  get_unique_characters
+from stats import count_words,  get_unique_characters, sorted_list
 
 def main():
     path = 'books/frankenstein.txt'
     text = get_book_text(path)
     number_of_words = count_words(text)
-    print(f'Found {number_of_words} total words')
     each_character_dict = get_unique_characters(text)
-    print(each_character_dict)
+    sorted_characters = sorted_list(each_character_dict)
+    print_menu(path, number_of_words, sorted_characters)
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
+
+def print_menu(path, number_of_words, sorted_characters):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("--------- Character Count -------")
+
+    for item in sorted_characters:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+        else:
+            continue
+    
+    print("============= END ===============")
 
 main()
