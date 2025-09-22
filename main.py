0b82b046d1c9b8c67643cb9f75d75a483d01aa4e
Saet1a <joaorogeroliveira@gmail.com>
2025-09-22 01:53:46 -0300

    Add function to read books

---
+++ main.py
@@ -0,0 +1,12 @@
+file_path = 'books/frankenstein.txt'
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    print(file_contents)
+
+
+def main():
+    get_book_text(file_path)
+
+main()
