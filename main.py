a486b756ba89170eadea7de05ee25f89796ec775
Saet1a <joaorogeroliveira@gmail.com>
2025-09-22 14:13:36 -0300

    Add function to count character appearances

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from stats import count_words
+from stats import count_words,  get_unique_characters
+
 def main():
     path = 'books/frankenstein.txt'
     text = get_book_text(path)
     number_of_words = count_words(text)
     print(f'Found {number_of_words} total words')
+    each_character_dict = get_unique_characters(text)
+    print(each_character_dict)
 
 def get_book_text(file_path):
     with open(file_path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def count_words(text_to_count):
     words_number = text_to_count.split()
     return len(words_number)
+
+def get_unique_characters(text):
+    dictionary_word_appearances = {}
+    for char in text.lower():
+        if char in dictionary_word_appearances:
+            dictionary_word_appearances[char] += 1
+        else:
+            dictionary_word_appearances[char] = 1        
+    return dictionary_word_appearances
